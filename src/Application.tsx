32cfb9433660f5bf2694f6f7e1371e6675fce2d6
Nikolay Karpuzov <niksfred95@gmail.com>
2022-09-06 11:28:15 +0300

    tailwind setup

---
+++ src/Application.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const Application = () => {
+  const [filter, setFilter] = useState([]);
+  return (
+    <div>
+      <div>
+        <header></header>
+        <main></main>
+      </div>
+    </div>
+  );
+};
+
+export default Application;
